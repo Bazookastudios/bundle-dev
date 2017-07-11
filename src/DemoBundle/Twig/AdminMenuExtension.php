468f4f4998f3b4a81acf0d0336f219d9c034ffc0
Robbe Clerckx <robbe@bazookas.be>
2017-07-11 15:30:52 +0200

    Updated bundles for using v2 admin controllers for ApiFramework AppCopy and AppSettings

---
+++ src/DemoBundle/Twig/AdminMenuExtension.php
@@ -0,0 +1,49 @@
+<?php
+namespace DemoBundle\Twig;
+
+use Bazookas\AdminBundle\AdminElements\Containers\MenuContainer;
+use Bazookas\AdminBundle\AdminElements\Elements\Actions\MenuActions\MenuActionElement;
+use Bazookas\AdminBundle\Twig\Base\BaseAdminMenuExtension;
+use Bazookas\CommonBundle\Security\Roles;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminMenuExtension extends BaseAdminMenuExtension
+{
+
+  protected function createMenu(Request $request)
+  {
+    parent::createMenu($request);
+
+    $this->addApiFrameworkBundleMenuItems();
+  }
+
+  /**
+   * Add the api framework bundle menu items
+   */
+  protected function addApiFrameworkBundleMenuItems() {
+//    $container = new MenuContainer([], [
+//      'label' => 'admin.menu.app.label',
+//      'roles' => [Roles::ROLE_SUPER_ADMIN],
+//      'iconClass' => 'ti ti-mobile'
+//    ]);
+
+    // add the copy item
+    $this->elements[] = (new MenuActionElement([], [
+      'label' => 'admin.entities.appCopy.namePlural',
+      'route' => 'bazookas_api_framework_app_copy_admin',
+      'iconClass' => 'fa fa-language',
+      'roles' => [Roles::ROLE_SUPER_ADMIN],
+    ]));
+
+    // add the app settings item
+//    $container->addChild(new MenuActionElement([], [
+//      'label' => 'admin.entities.appSettings.namePlural',
+//      'route' => 'bazookas_api_framework_app_settings_admin',
+//      'iconClass' => 'fa fa-gears',
+//      'roles' => [Roles::ROLE_SUPER_ADMIN],
+//    ]));
+
+    // add it to the elements array
+//    $this->elements[] = $container;
+  }
+}
