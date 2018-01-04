538f7d3bfa912aa7b6510b6735c24f4e9127c851
Simon Claeye <sclaeye@gmail.com>
2018-01-04 12:26:26 +0100

    Added admin element fixes to cron bundle

--- src/DemoBundle/Twig/AdminMenuExtension.php
+++ src/DemoBundle/Twig/AdminMenuExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 class AdminMenuExtension extends BaseAdminMenuExtension
 {
 
-  use CronMenuTrait;
+//  use CronMenuTrait;
   use MediaMenuTrait;
 
   /**
