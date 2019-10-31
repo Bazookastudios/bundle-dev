5a769d493210b6b57a768e982bb982915293efa7
Simon Claeye <sclaeye@gmail.com>
2019-10-31 11:37:42 +0100

    Adminbundle now works with v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,20 @@
-const Encore = require('./src/Bazookas/CommonBundle/common.config');
+const Encore = require('./bundles/Bazookas/CommonBundle/common.config');
 
 const WEBSITE_ROOT = './src/WebsiteBundle/Resources/public/';
 const DEMO_ROOT = './src/DemoBundle/Resources/public/';
 
-// will output as web/build/app.js
-Encore
-// will output as web/build/app.js
-  .addEntry('js/app', WEBSITE_ROOT + 'js/main.js')
-  .addEntry('js/custom_admin_components', DEMO_ROOT + 'js/admin/CustomAdminComponents.js')
+// Encore
+//   .addEntry('js/app', WEBSITE_ROOT + 'js/main.js').
+//   addEntry('js/custom_admin_components',
+//     DEMO_ROOT + 'js/admin/CustomAdminComponents.js')
+//   .addStyleEntry('css/app', WEBSITE_ROOT + 'css/main.scss');
 
-  // will output as web/build/global.css
-  .addStyleEntry('css/app', WEBSITE_ROOT + 'css/main.scss');
-
-// export the final configuration
 module.exports = function () {
   let config = Encore.getWebpackConfig();
 
   //This is needed for mysterious reasons
   config.node = {
-    fs: 'empty'
+    fs: 'empty',
   };
 
   return config;
