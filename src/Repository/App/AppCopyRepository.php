2f73e21192fdac87c62e461cb1d7995a87894b1a
Simon Claeye <sclaeye@gmail.com>
2019-11-21 11:48:25 +0100

    App copy repo now uses the bundle trait

--- src/Repository/App/AppCopyRepository.php
+++ src/Repository/App/AppCopyRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository\App;
 
 use Bazookas\AdminBundle\Repository\Base\BaseRepository;
 use Nitro\AppCopyBundle\Repository\Interfaces\AppCopyRepositoryInterface;
+use Nitro\AppCopyBundle\Repository\Traits\AppCopyRepositoryTrait;
 
 /**
  * Class AppCopyRepository
@@ -11,21 +12,5 @@ use Nitro\AppCopyBundle\Repository\Interfaces\AppCopyRepositoryInterface;
  */
 class AppCopyRepository extends BaseRepository implements AppCopyRepositoryInterface
 {
-    /**
-     * @param string $locale
-     *
-     * @return array
-     */
-    public function getAllCopy(string $locale): array
-    {
-        $qb = $this->createQueryBuilder('app_copy');
-        $qb
-            ->select('partial app_copy.{id}')
-            ->addSelect('partial detail.{id, value}')
-            ->join('app_copy.details', 'detail', 'WITH', $qb->expr()->eq('detail.locale', ':locale'))
-            ->setParameter('locale', $locale)
-        ;
-
-        return $qb->getQuery()->getArrayResult();
-    }
+    use AppCopyRepositoryTrait;
 }
