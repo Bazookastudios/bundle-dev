c0802df5b1798c1f1cf6e8cc5eec6f6a8af26eab
Simon Claeye <sclaeye@gmail.com>
2019-11-22 11:45:11 +0100

    Now using roles from Nitro bundles instead of admin bundle

--- src/Entity/App/AppCopy.php
+++ src/Entity/App/AppCopy.php
@@ -4,9 +4,10 @@ namespace App\Entity\App;
 
 use Bazookas\AdminBundle\Entity\Interfaces\AccessControlInterface;
 use Bazookas\AdminBundle\Entity\Traits\AccessControlTrait;
-use Bazookas\AdminBundle\Security\Roles;
 use Bazookas\CommonBundle\Entity\Interfaces\EntityInterface;
 use Bazookas\CommonBundle\Entity\Interfaces\TimestampableInterface;
+use DateTime;
+use Nitro\AppCopyBundle\Security\Roles;
 use Symfony\Component\Validator\Constraints as Assert;
 use Bazookas\CommonBundle\Entity\Traits\TimestampableTrait;
 use DateTimeImmutable;
@@ -26,7 +27,7 @@ use function get_class;
  */
 class AppCopy implements AppCopyInterface, EntityInterface, TimestampableInterface, AccessControlInterface
 {
-    public const DEFAULT_REQUIRED_ROLE = Roles::ROLE_ADMIN;
+    public const DEFAULT_REQUIRED_ROLE = Roles::ROLE_COPY_ADMIN;
 
     use AppCopyTrait;
     use TimestampableTrait;
@@ -40,7 +41,7 @@ class AppCopy implements AppCopyInterface, EntityInterface, TimestampableInterfa
     {
         $this->details = new ArrayCollection();
         $this->created = new DateTimeImmutable('now', new DateTimeZone('UTC'));
-        $this->modified = new DateTimeImmutable('now', new DateTimeZone('UTC'));
+        $this->modified = new DateTime('now', new DateTimeZone('UTC'));
     }
 
     /**
